25c4427c00901504d43046cf113daf973e875795
Hannah West <hannahtan.west@gmail.com>
2024-06-18 10:28:58 +0200

    billede rettelse

--- src/components/molecules/Vaeleur.jsx
+++ src/components/molecules/Vaeleur.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Vaeleur.module.css'
 import Image from 'next/image'
-import phoneMock from 'public/væleur-phone-mock.png'
+import phoneMock from 'public/vaeleur-phone-mock.png'
 import Link from 'next/link'
 import poster from 'public/væleur-collage.jpg'
 
